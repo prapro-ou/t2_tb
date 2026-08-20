bad5b5fcfca0f800596a28ec6f2b281af4fd754b
HATA Yoshihiro <powv9pbx@s.okayama-u.ac.jp>
2026-08-21 03:04:43 +0900

    Add symbol module prototype

--- Assets/Users/iiiiii/symbolButton.cs
+++ Assets/Users/iiiiii/symbolButton.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class symbolButton : MonoBehaviour
 {
+    public int symbolNumber;
+    public SymbolManager manager;
+
     private void OnMouseDown()
     {
         Debug.Log(gameObject.name + " が押されました");
+        manager.PressSymbol(symbolNumber);
     }
 }
