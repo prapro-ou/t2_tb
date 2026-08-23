a9d51e47f9cf352e5522c001760dcedb08cb6c11
HATA Yoshihiro <powv9pbx@s.okayama-u.ac.jp>
2026-08-24 04:35:07 +0900

    hanteikyouka

--- Assets/Users/iiiiii/SymbolManager.cs
+++ Assets/Users/iiiiii/SymbolManager.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
 public class SymbolManager : MonoBehaviour
 {
+    // 記号画像
     [SerializeField] private SpriteRenderer[] symbolSprites;
+
+    // ボタン本体
+    [SerializeField] private SpriteRenderer[] buttonSprites;
+
+    // 記号候補
     [SerializeField] private Sprite[] symbolCandidates;
 
     private int[] correctOrder;
@@ -15,6 +22,9 @@ public class SymbolManager : MonoBehaviour
 
         SetSymbols(settingData.SymbolIndices);
         correctOrder = settingData.CorrectOrder.ToArray();
+
+        // 開始時は青
+        SetButtonColor(Color.blue);
 
         Debug.Log("今回の正解順: " + string.Join(" → ", correctOrder));
     }
@@ -34,7 +44,7 @@ public class SymbolManager : MonoBehaviour
             symbolSprites[i].enabled = true;
             symbolSprites[i].sprite = symbolCandidates[index];
 
-            // 背景より前に表示
+            // ボタンより前に表示
             symbolSprites[i].sortingOrder = 10;
         }
     }
@@ -56,13 +66,42 @@ public class SymbolManager : MonoBehaviour
             if (currentIndex >= correctOrder.Length)
             {
                 Debug.Log("解除成功！");
+
+                // 全て正解したら緑
+                SetButtonColor(Color.green);
+
                 currentIndex = 0;
             }
         }
         else
         {
             Debug.Log("ミス！");
+
+            // 最初からやり直し
             currentIndex = 0;
+
+            // 一瞬赤くしてから青へ戻す
+            StartCoroutine(FlashRed());
+        }
+    }
+
+    private IEnumerator FlashRed()
+    {
+        // 赤にする
+        SetButtonColor(Color.red);
+
+        // 0.3秒間赤
+        yield return new WaitForSeconds(0.3f);
+
+        // 通常の青に戻す
+        SetButtonColor(Color.blue);
+    }
+
+    private void SetButtonColor(Color color)
+    {
+        for (int i = 0; i < buttonSprites.Length; i++)
+        {
+            buttonSprites[i].color = color;
         }
     }
 }
